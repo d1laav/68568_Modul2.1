4216b5f4b63a7a20b13066222d3bc0fb5ad4f3e7
d1laav <dimmanuel31@gmail.com>
2025-02-19 22:04:42 +0700

    4. Color schemes

--- app/src/main/java/com/example/reply/ui/MainActivity.kt
+++ app/src/main/java/com/example/reply/ui/MainActivity.kt
@@ -16,17 +16,19 @@
 
 package com.example.reply.ui
 
-import android.content.res.Configuration.UI_MODE_NIGHT_NO
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.reply.data.LocalEmailsDataProvider
+import com.example.reply.ui.theme.AppTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -52,18 +54,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Preview(
-    uiMode = UI_MODE_NIGHT_YES,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
     name = "DefaultPreviewDark"
 )
 @Preview(
-    uiMode = UI_MODE_NIGHT_NO,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
     name = "DefaultPreviewLight"
 )
 @Composable
-fun ReplyAppPreviewLight() {
-    ReplyApp(
-        replyHomeUIState = ReplyHomeUIState(
-            emails = LocalEmailsDataProvider.allEmails
-        )
-    )
+fun ReplyAppPreview() {
+    AppTheme {
+        Surface (tonalElevation = 5.dp) {
+            ReplyApp(
+                replyHomeUIState = ReplyHomeUIState(
+                    emails = LocalEmailsDataProvider.allEmails,
+                )
+            )
+        }
+    }
 }
+
+
